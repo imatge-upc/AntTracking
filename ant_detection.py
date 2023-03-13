04f015997a07270c849e85c76ab299854d2c26c6
Ignasi <ignasi@annamarco.com>
2023-03-13 17:42:26 +0100

    On branch Ignasi  Changes to be committed: 	new file:   DATA/output_4_gt.txt 	new file:   OUTPUT/ocsort_tracking_output_4.txt 	new file:   OUTPUT/output_4.txt 	deleted:    README.md 	deleted:    angular_error.py 	modified:   ant_detection.py 	new file:   docopts/__init__.py 	new file:   docopts/help_ant_detection.py 	new file:   docopts/help_ocsort_track.py 	new file:   docopts/help_sort_inference.py 	new file:   histogramas.ipynb 	new file:   metrics/__init__.py 	new file:   metrics/mot_ground_truth_assigner.py 	new file:   minimum_id.py 	new file:   models/__init__.py 	new file:   models/foreground_mask_object_detector.py 	new file:   models/ocsort_utils/__init__.py 	new file:   models/ocsort_utils/associator.py 	new file:   models/ocsort_utils/metric_utils.py 	new file:   models/ocsort_utils/ocsort_kalman_estimator.py 	new file:   models/ocsort_utils/track_manager.py 	new file:   models/sort.py 	new file:   models/sort_utils/__init__.py 	new file:   models/sort_utils/iou_associator.py 	new file:   models/sort_utils/reduced_bbox_kalman_estimator.py 	new file:   models/sort_utils/track_manager.py 	new file:   ocsort_track.py 	modified:   plot_rectangles_video.py 	new file:   sort_inference.py 	deleted:    tracking_utils.py

--- ant_detection.py
+++ ant_detection.py
@@ -1,114 +1,111 @@
-"""  
-Use background substraction to detect ants in a video
-  
-Usage:
-  ant_detection.py <inputVideo> <detectionFile> [--subsAlg=<sa>] [--varThreshold=<vt>] [--filterFG=<ff>] [--writeImages=<wi>] [--outputDir=<od>] [--minSize=<ms>] [--startWriteFrames=<sw>] [--stopFrame=<sf>]
-  ant_detection.py -h | --help
 
-Options:
-  --subsAlg=<sa>            Background substraction algorithm (KNN/MOG2)  [default: MOG2]
-  --varThreshold=<vt>       threshold [default: 15]
-  --filterFG=<ff>           Whether to apply foreground filtering [default: True]
-  --writeImages=<wi>        Whether to save the fg images and boxes [default: False]
-  --outputDir=<od>          Output folder where the images are stored [default: out]
-  --minSize=<ms>            Minimum size (in pixels) of the width or height of the object to be considered [default: 20]
-  --startWriteFrames=<sw>   Do not start writing frames until this threshold is reached [default: 500]
-  --endFrame<ef>            Stop processing at this frame [default: -1]
-"""
-
+from contextlib import contextmanager
 import cv2 as cv
-import argparse
-import numpy as np
-from docopt import docopt
-from distutils.util import strtobool
 import sys
 
-
-if __name__ == '__main__':
-    # read arguments
-    args = docopt(__doc__)
-    input_video    = args['<inputVideo>']
-    detection_file = args['<detectionFile>']
-
-    subs_alg       = args['--subsAlg']
-    var_thresh     = int(args['--varThreshold'])    
-    filter_fg      = bool(strtobool(args['--filterFG']))
-    write_images   = bool(strtobool(args['--writeImages']))
-    out_dir        = args['--outputDir']
-    min_size       = int(args['--minSize'])
-    start_write    = int(args['--startWriteFrames'])
-    stop_frame     = int(args['--stopFrame'])
+from docopts.help_ant_detection import parse_args
+from models.foreground_mask_object_detector import ForegroundMaskObjectDetector
 
 
-    if subs_alg == 'MOG2':
-        backSub = cv.createBackgroundSubtractorMOG2(varThreshold=var_thresh, detectShadows=False)
-    else:
-        backSub = cv.createBackgroundSubtractorKNN()
+@contextmanager
+def VideoCapture(input_video):
 
+    # findFileOrKeep allows more searching paths
     capture = cv.VideoCapture(cv.samples.findFileOrKeep(input_video))
     if not capture.isOpened():
         print('Unable to open: ' + input_video, file=sys.stderr)
         exit(0)
 
-    first = True
-    se1 = cv.getStructuringElement(cv.MORPH_RECT, (5,5))
-    se2 = cv.getStructuringElement(cv.MORPH_RECT, (2,2))
+    try:
+        yield capture
+    finally:
+        # Release the video capture object at the end
+        capture.release()
 
-    fr = 1
 
-    with open(detection_file, 'w') as out_file:
-        while True:
-            if stop_frame > 0 and fr > stop_frame:
-                break
-            
-            if fr%500 == 0:
-                print (f'Processing frame {fr}', file=sys.stderr)
-            ret, frame = capture.read()
-            if frame is None:
-                break
+class MorphologyNoiseFilter():
+    # Remove noise  https://stackoverflow.com/questions/30369031/remove-spurious-small-islands-of-noise-in-an-image-python-opencv
 
-            h,w,d = frame.shape
-            if first == True:
-                out_ima = np.zeros((h,2*w,d), dtype=np.uint8)
-                first = False
-                
-            fgMask = backSub.apply(frame)
-
-            if fr < 500:
-                fr = fr + 1
-                continue
+    def __init__(self):
+        self.se1 = cv.getStructuringElement(cv.MORPH_RECT, (5,5))
+        self.se2 = cv.getStructuringElement(cv.MORPH_RECT, (2,2))
     
-            # Remove noise  https://stackoverflow.com/questions/30369031/remove-spurious-small-islands-of-noise-in-an-image-python-opencv
-            if filter_fg:
-                fgMask = cv.morphologyEx(fgMask, cv.MORPH_CLOSE, se1)
-                fgMask = cv.morphologyEx(fgMask, cv.MORPH_OPEN,  se2)
-                
-            out_ima[:,0:w,:] = frame
-            out_ima[:,w:, 0] = fgMask
-            out_ima[:,w:, 1] = fgMask
-            out_ima[:,w:, 2] = fgMask
+    def __call__(self, fgMask):
+        fgMask = cv.morphologyEx(fgMask, cv.MORPH_CLOSE, self.se1)
+        fgMask = cv.morphologyEx(fgMask, cv.MORPH_OPEN,  self.se2)
+        return fgMask
 
+def bboxes_from_conected_components(fgMask, min_size=20, connectivity=4, ltype=cv.CV_32S):
+    analysis = cv.connectedComponentsWithStats(fgMask, connectivity, ltype)
+    (totalLabels, labels, stats, centroids) = analysis
 
-            analysis = cv.connectedComponentsWithStats(fgMask, 4, cv.CV_32S)
-            (totalLabels, labels, stats, centroids) = analysis
-
-
-            for ii in range(1, totalLabels):
-                # extract the connected component statistics for the current
-                # label
-                x    = stats[ii, cv.CC_STAT_LEFT]
-                y    = stats[ii, cv.CC_STAT_TOP]
-                w    = stats[ii, cv.CC_STAT_WIDTH]
-                h    = stats[ii, cv.CC_STAT_HEIGHT]
-                #area = stats[ii, cv.CC_STAT_AREA]
-                if w > min_size and h > min_size:
-                    #print (f'Warning: small object: x={x}, y={y}, w={w}, h={h}', file=sys.stderr)
-                    print (f'{fr}, -1, {x}, {y}, {w}, {h}, 1, -1, -1, -1', file=out_file)
-
-            if write_images and fr > start_write:
-                cv.imwrite (f'{out_dir}/out_{fr:06d}.png', out_ima)
+    bboxes = []
+    for ii in range(1, totalLabels):
+        # extract the connected component statistics for the current label
+        x = stats[ii, cv.CC_STAT_LEFT]
+        y = stats[ii, cv.CC_STAT_TOP]
+        w = stats[ii, cv.CC_STAT_WIDTH]
+        h = stats[ii, cv.CC_STAT_HEIGHT]
         
-            fr = fr + 1
+        if w > min_size and h > min_size:
+            bboxes.append((x, y, w, h))
+    
+    return bboxes
 
-    # Release the video capture and writer objects
-    capture.release()
+if __name__ == '__main__':
+    # read arguments
+    input_video, detection_file, subs_alg,\
+    var_thresh, filter_fg, write_images,\
+    out_dir, min_size, start_write, stop_frame = parse_args(sys.argv)
+
+
+    # Define the Background Subtractor function
+    if subs_alg == 'MOG2':
+        backSub = cv.createBackgroundSubtractorMOG2(varThreshold=var_thresh, detectShadows=False)
+    else:
+        backSub = cv.createBackgroundSubtractorKNN()
+    backSub_func = lambda frame : backSub.apply(frame)
+
+    # Define the Noise Filter
+    if filter_fg:
+        filter_fg_func = MorphologyNoiseFilter()
+    else:
+        filter_fg_func = None
+
+    # Define the bboxes Finder
+    bbox_extractor = lambda fgMask : bboxes_from_conected_components(fgMask, min_size=min_size)
+
+    # Define debug function
+    if write_images:
+        def write_images(out_ima):
+            cv.imwrite (f'{out_dir}/out_{fr:06d}.png', out_ima)
+
+
+    # Ensamble the Detector
+    detector_model = ForegroundMaskObjectDetector(backSub_func, bbox_extractor, filter_fg_func, start_write, write_images)
+
+
+    # Apply the model
+    fr = 0
+    with VideoCapture(input_video) as capture:
+        with open(detection_file, 'w') as out_file:
+            while stop_frame <= 0 or fr <= stop_frame:
+                fr = fr + 1
+                
+                if fr % (start_write or 1) == 0:
+                    print (f'Processing frame {fr}', file=sys.stderr)
+
+                _, frame = capture.read()
+                if frame is None:
+                    print (f'Frame {fr} is None', file=sys.stderr)
+                    break
+                    
+                bboxes = detector_model(frame)
+                if bboxes is None:
+                    continue # Training Background
+                
+                if len(bboxes) > 0:
+                    # bbox = (x1, y1, w, h)
+                    MOTDet_line = lambda fr, bbox : f'{fr}, -1, {bbox[0]}, {bbox[1]}, {bbox[2]}, {bbox[3]}, 1, -1, -1, -1'
+                    detection_text = '\n'.join([MOTDet_line(fr, bbox) for bbox in bboxes])
+                    print (detection_text, file=out_file)
