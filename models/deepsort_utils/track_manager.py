441c6eb6a507408b44e90aa6e4abe3f52143868c
Ignasi <ignasi@annamarco.com>
2023-04-09 21:36:56 +0200

    On branch Ignasi_new  Changes to be committed: 	modified:   OUTPUT/deepsort_tracking_output_4.txt 	modified:   deepsort_track.py 	modified:   hota_idf1.ipynb 	deleted:    models/deepsort_utils/apparence_scorer.py 	modified:   models/deepsort_utils/associator.py 	new file:   models/deepsort_utils/nn_apparence_scorer.py 	modified:   models/deepsort_utils/track_manager.py

--- models/deepsort_utils/track_manager.py
+++ models/deepsort_utils/track_manager.py
@@ -5,16 +5,14 @@ import numpy as np
 class Track():
 
     instance_count = 0
-    
-    memory_size = 100
 
     @classmethod
     def reset(cls):
         cls.instance_count = 0
 
-    def __init__(self, estimator, det):
+    def __init__(self, estimator, apparence_scorer):
         self.estimator = estimator
-        self.apparence_memory = [det[5:].reshape(1, -1)]
+        self.apparence_scorer = apparence_scorer
 
         # identify the instance with a unique ID for dead and alive tracks (starts with 1 for MOT)
         Track.instance_count += 1
@@ -41,35 +39,40 @@ class Track():
         self.time_since_update += 1
         self.history.append(prediction)
 
-        return np.concatenate((prediction, *self.apparence_memory, np.full((1, 1), self.age)), axis=1) # 1, 5+4x4+Nx50+1
+        return np.concatenate((prediction, np.full((1, 1), self.age)), axis=1) # 1, 5+4x4+1
     
     def update(self, observation):
         output = self.estimator.update(observation)
         if observation is not None:
+            self.apparence_scorer.update(observation)
+
             self.history = []
             self.time_since_update = 0
 
             self.hits += 1
             self.hit_streak += 1
 
-            self.apparence_memory.append(observation[5:].reshape(1, -1))
-            self.apparence_memory = self.apparence_memory[-self.memory_size:]
-
             return output
         return self.history[-1]
     
-    __call__ = predict
+    def __getitem__(self, key):
+        return self.history[key]
+    
+    def __call__(self, query):
+        return self.apparence_scorer(query)
 
 class TrackManager():
 
-    @classmethod
-    def set_track_memory(cls, memory_size):
-        Track.memory_size = memory_size
-
-    def __init__(self, estiamtor_cls, max_age=1, min_hits=3, max_last_update=1, memory_size=100):
+    def __init__(self, estiamtor_cls, apparence_scorer_cls, track_cls=None, max_age=1, min_hits=3, max_last_update=1, return_pred=False):
 
         self.estiamtor_cls = estiamtor_cls
-        self.set_track_memory(memory_size)
+        self.apparence_scorer_cls = apparence_scorer_cls
+
+        self.track_cls = track_cls
+        if track_cls is None:
+            self.track_cls = Track
+
+        self.return_pred = return_pred
 
         self.max_age = max_age
         self.min_hits = min_hits
@@ -82,33 +85,44 @@ class TrackManager():
         self.trackers = []
         self.frame_count = 0
 
-    def get_track(self, key):
-        return self.trackers[key]
+    def get_track(self, key, predict=True):
+        track = self.trackers[key]
+        if predict:
+            track.predict()
+        return track
+    
+    def chk_output(self, trk):
+        last_update_criteria = trk.time_since_update < self.max_last_update
+        hit_streak_criteria = trk.hit_streak >= self.min_hits
+        initial_frames_criteria = self.frame_count <= self.min_hits
 
-    def manage_tracks(self, detections, estimations, matches):
+        return last_update_criteria and (hit_streak_criteria or initial_frames_criteria)
+
+    def manage_tracks(self, detections, matches):
         # estimations is a list of predictions np.array([x1, y1, x2, y2, score]).reshape((1, 5))
         # detections is a np.array([[x1, y1, x2, y2, score], ...]).reshape(N, 5)
         self.frame_count += 1
 
         unmatched_detections = np.setdiff1d(np.arange(len(detections)), matches[:, 0], assume_unique=True)
-        unmatched_estimations = np.setdiff1d(np.arange(len(estimations)), matches[:, 1], assume_unique=True)
+        unmatched_estimations = np.setdiff1d(np.arange(len(self.trackers)), matches[:, 1], assume_unique=True)
 
         output = []
         for i_det, i_trck in matches:
             trk = self.trackers[int(i_trck)]
             det = detections[int(i_det)]
 
-            pred_out = trk.update(det)
+            pred_out = trk[-1] if self.return_pred else det
+            trk.update(det)
 
             # Prepare output from active tracks
-            if (trk.time_since_update < self.max_last_update) and ((trk.hit_streak >= self.min_hits) or (self.frame_count <= self.min_hits)):
+            if self.chk_output(trk):
                 output.append(np.concatenate((pred_out[..., :4].reshape(-1), [trk.id])).reshape(1, -1))
         
         # Make a mirror VIEW of unmatched_estimations so the sorting is descending in the actual unmatched_estimations, so pop won't mess the indexes
         unmatched_estimations[::-1].sort()
         for i_trck in unmatched_estimations:
-            pred = estimations[int(i_trck)]
             trk = self.trackers[int(i_trck)]
+            pred = trk[-1]
 
             if np.any(~(np.isfinite(pred))) or trk.time_since_update > self.max_age:
                 # remove invalid trackers
@@ -117,17 +131,17 @@ class TrackManager():
             else:
                 pred_out = pred
 
-                if (trk.time_since_update < self.max_last_update) and ((trk.hit_streak >= self.min_hits) or (self.frame_count <= self.min_hits)):
+                if self.chk_output(trk):
                     output.append(np.concatenate((pred_out[..., :4].reshape(-1), [trk.id])).reshape(1, -1))
 
         # Create new Tracks and Prepare output if needed
         for i_det in unmatched_detections:
             det = detections[int(i_det)]
 
-            trk = Track(self.estiamtor_cls(det), det)
+            trk = self.track_cls(self.estiamtor_cls(det), self.apparence_scorer_cls(det))
             self.trackers.append(trk)
 
-            if (trk.time_since_update < self.max_last_update) and ((trk.hit_streak >= self.min_hits) or (self.frame_count <= self.min_hits)):
+            if self.chk_output(trk):
                 output.append(np.concatenate((det[:4], [trk.id])).reshape(1, -1))
 
         if len(output) > 0:
